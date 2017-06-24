3f52a076d1e656d2080e91a42e690fa81ec74134
HippErger <berger.clayton@gmail.com>
2017-06-24 14:53:41 -0500

    hard coded a lesson into lesson view page

--- src/pages/lessonview.js
+++ src/pages/lessonview.js
@@ -1,11 +1,97 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class LessonView extends Component {
-  render() {
-    return (
-      <div>This is the lesson view page</div>
-    );
-  }
+// // class LessonView extends Component
+// function Lessonview() {
+//   // render() {
+//     // @TODO add lesson header, like, addTo, copy
+//     return (
+//       <div>
+//         <h4><
+//       </div>
+//     );
+//   // }
+// }
+//
+// export default LessonView;
+
+function LessonView() {
+  return (
+    <div>
+      <h2>Title: Miming</h2>
+      <h4>TEKS:</h4>
+      <p>
+        1C -Develop and practice stage movement and techniques(such as mime,
+          pantomime, stage combat, Laban, LeCoq, or Viewpoints) consistently to
+          express thoughts, feelings, and actions non-verbally.
+
+        2E 	Employ physical techniques consistently to express thoughts,
+        feelings and actions non-verbally.
+      </p>
+      <h4>Material:</h4>
+        <p>Practice Your Miming</p>
+      <h4>Overview</h4>
+          <p>Be learning miming</p>
+      <h4>Objectives</h4>
+        <p>
+          Students will demonstrate the ability to use their whole bodies to convey action
+          and interaction with objects.
+        </p>
+      <h4>Warm-Up</h4>
+        <p>
+          -Stretch, yawn, buzz,
+          -Mirror, shadow, mesh into 4 people following 1 then leave that as a group
+        </p>
+      <h4>Instruction 1</h4>
+        <p>
+          <b>What is Pantomime?</b>
+          -Without any words, gesture for the students to make a seated circle
+          on the stage.<br />
+          -Gesture to them to begin silently copying (mirroring) your movement.
+          <br />
+          -Begin a pantomime of your choice (i.e. Waking up in the morning,
+            brushing your teeth, opening the fridge and pouring yourself a bowl
+            of cereal). Be precise and detailed.<br />
+          -At the end, break the silence and ask the students if they could
+          guess what you were doing.<br />
+          -How were you able to guess the activities? What helped you figure it
+          out?<br />
+          -Many times as actors, we have to make the audience believe certain
+          things exist that aren’t really there. This is called pantomime.<br />
+          -Give an example of poor pantomime, brushing your teeth without
+          clarity.<br />
+          -Which paints a better picture, that or what I did previously? In
+          pantomime, every movement should be precise, have a purpose, and help
+          tell the story. <br />
+          -Ask the students to try a Morning themed pantomimed scene while
+          mirroring another.<br />
+        </p>
+      <h4>Insstruction 2</h4>
+        <p>
+          <b>Do Pantomime?</b>
+          -Without any words, gesture for the students to make a seated circle
+          on the stage.<br />
+          -Gesture to them to begin silently copying (mirroring) your movement.
+          <br />
+          -Begin a pantomime of your choice (i.e. Waking up in the morning,
+            brushing your teeth, opening the fridge and pouring yourself a bowl
+            of cereal). Be precise and detailed.<br />
+          -At the end, break the silence and ask the students if they could
+          guess what you were doing.<br />
+          -How were you able to guess the activities? What helped you figure it
+          out?<br />
+          -Many times as actors, we have to make the audience believe certain
+          things exist that aren’t really there. This is called pantomime.<br />
+          -Give an example of poor pantomime, brushing your teeth without
+          clarity.<br />
+          -Which paints a better picture, that or what I did previously? In
+          pantomime, every movement should be precise, have a purpose, and help
+          tell the story. <br />
+          -Ask the students to try a Morning themed pantomimed scene while
+          mirroring another.<br />
+        </p>
+    </div>
+
+  );
 }
 
 export default LessonView;
