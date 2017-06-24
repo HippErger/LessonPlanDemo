519ffb32193d27246e21c00ce9aaa3eb7f47e603
HippErger <berger.clayton@gmail.com>
2017-06-24 14:13:40 -0500

    added basic layout to fan view

--- src/pages/fanview.js
+++ src/pages/fanview.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import FollowingThumbnail from '../components/followingThumbnail';
+import Header from '../components/header';
+import SubjectsThumbnail from '../components/subjectsThumbnail';
+
 
 
 class FanView extends Component {
   render() {
+    //  @TODO will need to map over the number of classes & followings of the user
+    // has to create the thumbnail for each one.
     return (
       <div>
-        <h3>This is the Fanview page</h3>
+        <Header />
+        <h3>Name of followed Teacher</h3>
+          <SubjectsThumbnail />
+          <hr />
+          <h6>This will be aligned to the right with followingThumbnails</h6>
+          <FollowingThumbnail />
       </div>
     );
   }
