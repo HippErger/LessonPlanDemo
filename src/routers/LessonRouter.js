9b617c4d2ffe0fc3b18ec03b73c380ec613f5b3a
HippErger <berger.clayton@gmail.com>
2017-06-25 12:54:20 -0500

    add const in LessonRouter to play with different pathnames

--- src/routers/LessonRouter.js
+++ src/routers/LessonRouter.js
@@ -2,7 +2,14 @@ import express from 'express';
 import LessonController from '../controllers/LessonController';
 
 const router = express.Router();
+/* eslint-disable no-unused-vars */
 const LessonsPath = '/api/lessonplans';
+const teacherName = 'yonderWay';
+const subjectName = 'theatre1';
+const lessonNumber = '/:id';
+
+
+// '/api/teachers/{name}/{class}/{lesson#}'
 
 router.post(LessonsPath, LessonController.create);
 router.get(LessonsPath, LessonController.list);
