5606a8626d7ea5725ea5ae38a687a569f0e733ee
HippErger <berger.clayton@gmail.com>
2017-06-26 18:33:42 -0500

    added actions but not connected yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import 'isomorphic-fetch';
 
+/* eslint-disable no-unused-vars */
 /* ///////////////////////////////////////////////////////////////////////////
                         Update Lesson Details
 /////////////////////////////////////////////////////////////////////////// */
@@ -28,3 +29,37 @@ export const LOAD_ONE_LESSON_FAILURE = 'LOAD_ONE_LESSON_FAILURE';
 export const LOAD_LESSON_LIST = 'LOAD_LESSON_LIST';
 export const LOAD_LESSON_LIST_SUCCESS = 'LOAD_LESSON_LIST_SUCCESS';
 export const LOAD_LESSON_LIST_FAILURE = 'LOAD_LESSON_LIST_FAILURE';
+
+export function loadLessonList() {
+  return dispatch => {
+    dispatch({
+      type: LOAD_LESSON_LIST,
+    });
+
+    fetch('/api/lessonplans')
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      dispatch(loadLessonListSuccess(data));
+    })
+    .catch(err => {
+      dispatch(loadLessonListFailure(err.message));
+      // add in next();
+    });
+  };
+}
+
+function loadLessonListSuccess(receivedData) {
+  return {
+    type: LOAD_LESSON_LIST_SUCCESS,
+    receivedData
+  };
+}
+
+function loadLessonListFailure(errorMessage) {
+  return {
+    type: LOAD_LESSON_LIST_FAILURE,
+    errorMessage
+  };  
+}
