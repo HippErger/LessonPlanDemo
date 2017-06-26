67672464395b07b1de7558380170c3ba458910c4
HippErger <berger.clayton@gmail.com>
2017-06-26 18:46:06 -0500

    added root reducer, list reducer and store but not connected to front yet

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import lessonListReducer from './lessonListReducer';
+
+export default combineReducers({
+  lessonListReducer,
+});
---
+++ src/reducers/lessonListReducer.js
@@ -0,0 +1,41 @@
+import {combineReducers} from 'redux';
+import {
+  LOAD_LESSON_LIST,
+  LOAD_LESSON_LIST_SUCCESS,
+  LOAD_LESSON_LIST_FAILURE
+} from '../actions';
+
+function loading(state = false, action) {
+  switch (action.type) {
+    case LOAD_LESSON_LIST:
+      return true;
+    case LOAD_LESSON_LIST_SUCCESS:
+    case LOAD_LESSON_LIST_FAILURE:
+      return false;
+  }
+  return state;
+}
+
+/* eslint-disable no-unused-vars */
+// maybe need to do something with action here: + action.message
+function error(state = null, action) {
+  return state;
+}
+
+function items(state = [], action) {
+  switch (action.type) {
+    case LOAD_LESSON_LIST_SUCCESS:
+      return action.receivedData;
+    case LOAD_LESSON_LIST_FAILURE:
+      return [];
+  }
+  return state;
+}
+
+const lessonListReducer = combineReducers({
+  loading,
+  error,
+  items
+});
+
+export default lessonListReducer;
